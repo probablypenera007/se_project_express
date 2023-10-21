e312c228bd915a72308b66bef48fbc085ddda14a
James Penera <jamespenera@icloud.com>
2023-10-21 08:29:58 -0700

    mongoose

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
-const app = express();
+const mongoose = require('mongoose');
 
 const { PORT = 3001 } = process.env;
+const app = express();
+mongoose.connect('mongodb://localhost:27017/wtwr_db');
 
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
