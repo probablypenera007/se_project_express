cea4639cf6a4f5cad6a9df87363509ffddbc7e87
James Penera <jamespenera@icloud.com>
2023-10-21 17:44:52 -0700

    schema

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect('mongodb://localhost:27017/wtwr_db');
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port ${PORT}`);
---
+++ models/clothingItem.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isUrl(v),
+      message: 'Link user input is invalid',
+    },
+  },
+});
