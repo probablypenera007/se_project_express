0f75a3e884da7e18826b059520b997f1e3afabc3
James Penera <jamespenera@icloud.com>
2023-10-22 17:41:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const { PORT = 3001 } = process.env;
 const app = express();
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db',() => {
+// mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db',() => {
+  mongoose.connect('mongodb://localhost:27017/wtwr_db',() => {
 console.log('CONNECTED TO DB!')}, e => console.log("DB NOT CONNECTED",e));
 
 
