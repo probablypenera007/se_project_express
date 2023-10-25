9aaee507ea2bab32b14b3a40286dd03befdd5c7e
James Penera <jamespenera@icloud.com>
2023-10-25 14:55:02 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const clothingItem = require('./clothingItem');
 router.use('/items', clothingItem);
 
 router.use((req, res) => {
-  res.status(500).send({ message: 'error in index.js route error' });
+  res.status(500).send({
+    "message": "Requested resource not found"
+  });
 });
 module.exports = router;
