f770fc146a25e937b3c07980f62869b89301a6a7
James Penera <jamespenera@icloud.com>
2023-10-25 15:49:17 -0700

    user schema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -4,11 +4,14 @@ const validator = require('validator');
 const clothingItem = new mongoose.Schema({
   name: {
     type: String,
+    minlength: 2,
+    maxlength: 30,
     required: true,
   },
   weather: {
     type: String,
     required: true,
+    enum: ['hot', 'warm', 'cold'],
   },
   imageUrl: {
     type: String,
