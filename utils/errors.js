e38fe101f93b605028ce9427c3920457538a1ad7
James Penera <jamespenera@icloud.com>
2023-10-26 07:27:58 -0700

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -5,6 +5,11 @@ module.exports = {
 };
 
 
+// res.status(BAD_REQUEST).send({
+//   message:"Requested resource not found",
+// })
+
+
 // 400 — invalid data passed to the methods for creating an item/user or updating an item, or invalid ID passed to the params.
 // 404 — there is no user or clothing item with the requested id, or the request was sent to a non-existent address.
 // 500 — default error. Accompanied by the message: "An error has occurred on the server."
