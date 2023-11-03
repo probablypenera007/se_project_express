d9e2df7f655652ce10bec31dbd611f7177fd9033
James Penera <jamespenera@icloud.com>
2023-11-02 19:27:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ const { PORT = 3001 } = process.env;
 const app = express();
 app.use(helmet());
 
-mongoose.connect("mongodb://localhost:27017/wtwr_db", () => {
-  console.log('CONNECTED TO DB!')}, e => console.log("DB NOT CONNECTED",e));
+mongoose.connect("mongodb://localhost:27017/wtwr_db")
+// ,
+//  () => {
+//   console.log('CONNECTED TO DB!')}, e => console.log("DB NOT CONNECTED",e));
 
 app.use(cors());
 app.use(express.json());
