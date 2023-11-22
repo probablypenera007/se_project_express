cebe4b4b2cb536213a4af06df4e23d46e9ae4d60
probablypenera007 <jamespenera@icloud.com>
2023-11-22 06:32:44 -0800

    Update users.js
    testing if my code have authorization issue

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
-const { auth } = require("../middlewares/auth");
+// const { auth } = require("../middlewares/auth");
 const {  validateId } = require("../middlewares/validation");
 const { getCurrentUsers, updateUser } = require("../controllers/users");
 
 
 // Read
-router.get("/me", auth,validateId, getCurrentUsers);
+router.get("/me",
+// auth,
+ validateId, getCurrentUsers);
 
 // Update
-router.patch("/me", auth, validateId, updateUser);
+router.patch("/me",
+// auth,
+validateId, updateUser);
 
 module.exports = router;
