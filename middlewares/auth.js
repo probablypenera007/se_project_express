daf2453bcc938ad23cf43f01ea6541924770daa8
probablypenera007 <jamespenera@icloud.com>
2023-11-22 21:32:45 -0800

    env set up

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const UnauthorizedError = require("../errors/unauthorized-err");
--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,12 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const {
   // JWT_SECRET: process.env.JWT_SECRET
+  NODE_ENV,
   JWT_SECRET
 } = process.env;
 
 
-export default JWT_SECRET;
+module.exports = { NODE_ENV, JWT_SECRET };
 
 // const { PORT = 3001 } = process.env;
