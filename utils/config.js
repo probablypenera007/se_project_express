b80e14c5c5c06aba04c7a245516d5dd266ccdb5a
probablypenera007 <jamespenera@icloud.com>
2023-11-22 16:17:26 -0800

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 
-module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET
-};
+const {
+  // JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET
+} = process.env;
+
+
+export default JWT_SECRET;
+
+// const { PORT = 3001 } = process.env;
